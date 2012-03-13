78b4ba566e16abfbac982dfdbe485552369f6355
Iwer Petersen <iwer.petersen@googlemail.com>
2012-03-13 10:16:28 +0100

    Added White Room

--- src/de/haw/hamburg/inf/pro/ws11/petersenwienrich/Config.java
+++ src/de/haw/hamburg/inf/pro/ws11/petersenwienrich/Config.java
@@ -2,19 +2,19 @@ package de.haw.hamburg.inf.pro.ws11.petersenwienrich;
 
 public class Config {
     /**
-     * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * TODO: EDIT THIS TO SET CORRECT LOCATIONS WHEN FILES NOT FOUND
-     * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TODO: EDIT THIS TO SET CORRECT LOCATIONS
+     * WHEN FILES NOT FOUND !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      */
 
-    private static final String PROJECT_HOME = "C:\\Users\\Umbriel\\workspace\\Phoenix3D\\";
+    // private static final String PROJECT_HOME = "C:\\Users\\Umbriel\\workspace\\Phoenix3D\\";
+
+    private static final String PROJECT_HOME = "C:\\Users\\elresidente\\git\\interactive-installations-phoenix3d\\";
     // private static final String PROJECT_HOME =
     // "/home/elresidente/Develop/Repository/interactive-installations-phoenix3d/";
 
     /* ########################################################### */
 
-    public static final String IMAGES_LOCATION = PROJECT_HOME
-            + "resources\\images\\";
+    public static final String IMAGES_LOCATION = PROJECT_HOME + "resources\\images\\";
     // public static final String IMAGES_LOCATION = PROJECT_HOME +
     // "resources/images/";
 
@@ -33,6 +33,13 @@ public class Config {
     public static final int COLOR_DUST = 204;
 
     public static final int COLOR_MODE = COLOR_FULL;
+
+    /* ########################################################### */
+    // Room Mode Constants
+    public static final int ROOM_SHABY = 300;
+    public static final int ROOM_WHITE = 301;
+
+    public static final int ROOM_MODE = ROOM_SHABY;
 
     /* ########################################################### */
     // ParticleConstants
